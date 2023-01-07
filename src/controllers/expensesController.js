8c210f22c59217fedea6d0829842655c2f980a94
cellamarematias <cellamarematias@gmail.com>
2023-01-07 15:23:20 -0300

    Fixed expenses controller - balance to 0.

--- src/controllers/expensesController.js
+++ src/controllers/expensesController.js
@@ -2,9 +2,9 @@ import { pool } from "../db.js";
 
 
 export const getExpenses = async (req, res) => {
-    try {
-        const [rows] = await pool.query('SELECT * FROM gastos ORDER BY date DESC LIMIT 50');
-        res.send(rows);
+	try {
+		const [rows] = await pool.query('SELECT * FROM gastos ORDER BY date DESC LIMIT 50');
+		res.send(rows);
 	} catch (error) {
 		return res.status(500).json({
 			message: 'Something went wrong.'
@@ -13,17 +13,17 @@ export const getExpenses = async (req, res) => {
 };
 
 export const createExpense = async (req, res) => {
-    const {descripcion, monto, id_usuario, date, total } = req.body;
+	const { descripcion, monto, id_usuario, date, total } = req.body;
 	console.log(req.body)
 
-    try {
-        const [rows] = await pool.query('INSERT INTO gastos (descripcion, monto, id_usuario, date, total) VALUES (?, ?, ?, ?, ?)', [descripcion, monto, id_usuario, date, total]);
-        res.send({
-            id: rows.insertId,
-            descripcion,
-            monto,
-            id_usuario
-        });
+	try {
+		const [rows] = await pool.query('INSERT INTO gastos (descripcion, monto, id_usuario, date, total) VALUES (?, ?, ?, ?, ?)', [descripcion, monto, id_usuario, date, total]);
+		res.send({
+			id: rows.insertId,
+			descripcion,
+			monto,
+			id_usuario
+		});
 	} catch (error) {
 		return res.status(500).json({
 			message: error
@@ -36,17 +36,32 @@ export const updateExpense = async (req, res) => {
 	const { id } = req.params;
 	const { descripcion, monto, date, id_usuario } = req.body;
 
-	try {
-		const [result] = await pool.query('UPDATE gastos SET descripcion = IFNULL(?, descripcion), monto = IFNULL(?, monto), date = IFNULL(?, date), id_usuario = IFNULL(?, id_usuario) WHERE idgastos = ?', [descripcion, monto, date, id_usuario, id]);
-		if (result.affectedRows === 0) return res.status(404).json({
-			message: 'Expense not found'
-		})
-		const [rows] = await pool.query('SELECT * FROM gastos WHERE idgastos = ?', [id]);
-		res.send(rows[0]);
-	} catch (error) {
-		return res.status(500).json({
-			message: 'Expense not found'
-		})
+	if (descripcion === 'Balance a $0') {
+		try {
+			const [result] = await pool.query('UPDATE gastos SET descripcion = IFNULL(?, descripcion), monto = IFNULL(?, monto), date = IFNULL(?, date), id_usuario = IFNULL(?, id_usuario), , pagado = IFNULL(?, pagado) WHERE idgastos = ?', [descripcion, monto, date, id_usuario, true, id]);
+			if (result.affectedRows === 0) return res.status(404).json({
+				message: 'Expense not found'
+			})
+			const [rows] = await pool.query('SELECT * FROM gastos WHERE idgastos = ?', [id]);
+			res.send(rows[0]);
+		} catch (error) {
+			return res.status(500).json({
+				message: 'Expense not found'
+			})
+		}
+	} else {
+		try {
+			const [result] = await pool.query('UPDATE gastos SET descripcion = IFNULL(?, descripcion), monto = IFNULL(?, monto), date = IFNULL(?, date), id_usuario = IFNULL(?, id_usuario) WHERE idgastos = ?', [descripcion, monto, date, id_usuario, id]);
+			if (result.affectedRows === 0) return res.status(404).json({
+				message: 'Expense not found'
+			})
+			const [rows] = await pool.query('SELECT * FROM gastos WHERE idgastos = ?', [id]);
+			res.send(rows[0]);
+		} catch (error) {
+			return res.status(500).json({
+				message: 'Expense not found'
+			})
+		}
 	}
 };
 
